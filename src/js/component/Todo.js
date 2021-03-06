f4e164e15caa6964d8164bb7f1325eb635561c71
toxigeno <melissaaraya@yahoo.com>
2021-03-06 03:01:52 +0000

    inputs funcionan

--- src/js/component/Todo.js
+++ src/js/component/Todo.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Todo() {
+	const [task, setTask] = useState("");
+	const [listTask, setListTask] = useState([]);
+
+	const putTask = () => {
+		if (task != "") {
+			setListTask([...listTask, task]);
+		}
+	};
+
 	return (
 		<div className="container">
 			<div className="todo-box">
 				<h1>To Do</h1>
-				<div className="input-group mb-3">
+				<div className="input-group">
 					<input
 						type="text"
 						className="form-control"
 						placeholder="add new task"
+						onChange={e => {
+							setTask(e.target.value);
+						}}
 					/>
 					<div className="input-group-append">
 						<button
+							onClick={putTask}
 							className="btn btn-outline-secondary"
 							type="button">
 							confirm
@@ -20,7 +33,13 @@ function Todo() {
 					</div>
 				</div>
 				<ul className="list-group">
-					<li className="list-group-item">task 1</li>
+					{listTask.map((item, index) => {
+						return (
+							<li key={index} className="list-group-item">
+								{item}
+							</li>
+						);
+					})}
 				</ul>
 				<small className="text-muted">Mucho por hacer</small>
 			</div>
