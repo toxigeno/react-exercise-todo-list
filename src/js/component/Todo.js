e397d5a662295cf3064ce8691ab6d0a815270e0b
toxigeno <melissaaraya@yahoo.com>
2021-03-09 05:59:08 +0000

    falta hover

--- src/js/component/Todo.js
+++ src/js/component/Todo.js
@@ -20,47 +20,51 @@ function Todo() {
 
 	return (
 		<div className="container">
-			<div className="todo-box">
-				<h1>To Do</h1>
-				<div className="input-group">
-					<input
-						type="text"
-						className="form-control"
-						placeholder="add new task"
-						onChange={e => {
-							setTask(e.target.value);
-						}}
-						value={task}
-					/>
-					<div className="input-group-append">
-						<button
-							onClick={putTask}
-							className="btn btn-outline-secondary"
-							type="button">
-							confirm
-						</button>
+			<div className="todo-box row p-3">
+				<div className="col">
+					<h1>To Do</h1>
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="add new task"
+							onChange={e => {
+								setTask(e.target.value);
+							}}
+							value={task}
+						/>
+						<div className="input-group-append">
+							<button
+								onClick={putTask}
+								className="btn btn-outline-secondary"
+								type="button">
+								confirm
+							</button>
+						</div>
 					</div>
+					<ul className="list-group">
+						{listTask.map((item, index) => {
+							return (
+								<li
+									key={index}
+									className="list-group-item list-group-item-action">
+									{item}
+									<button
+										onClick={() => {
+											deleteTask(index);
+										}}
+										type="button"
+										className="btn btn-secondary hide">
+										x
+									</button>
+								</li>
+							);
+						})}
+					</ul>
+					<small className="text-muted">
+						{listTask.length} tareas por hacer
+					</small>
 				</div>
-				<ul className="list-group">
-					{listTask.map((item, index) => {
-						return (
-							<li key={index} className="list-group-item">
-								{item}
-								<button
-									onClick={() => {
-										deleteTask(index);
-									}}
-									type="button"
-									className="btn btn-secondary">
-									x
-								</button>
-							</li>
-						);
-					})}
-				</ul>
-				<small className="text-muted">
-					{listTask.length} tareas por hacer
-				</small>
 			</div>
 		</div>
 	);
